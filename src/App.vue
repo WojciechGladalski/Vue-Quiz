61fe2d9056d409c4ba04f1c00f5288018a59d1f8
Wojciech <wojciech.gladalski@gmail.com>
2019-12-22 18:01:23 +0100

    main vue file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header
+        v-bind:numCorrect="numCorrect"
+        v-bind:numTotal="numTotal"
+    />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="8" offset="2">
+          <QuestionBox
+                  v-if="questions.length"
+                  v-bind:currentQuestion="questions[index]"
+                  v-bind:next="next"
+                  v-bind:increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+        if (isCorrect) {
+            this.numCorrect++
+        }
+        this.numTotal++
+    }
+  },
+  mounted: function () {
+    fetch('https://opentdb.com/api.php?amount=10&category=23&difficulty=hard&type=multiple', {
+      method: 'get'
+    })
+            .then((response) => {
+              let jsonResponse = response.json()
+              // eslint-disable-next-line no-console
+              console.log(jsonResponse)
+              return jsonResponse
+            })
+            .then((jsonData) => {
+              this.questions = jsonData.results
+            })
   }
 }
 </script>
