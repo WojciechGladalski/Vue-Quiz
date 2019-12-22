b07598131b506425a0aa8637ebdff595e13f283c
Wojciech <wojciech.gladalski@gmail.com>
2019-12-22 18:02:30 +0100

    added header and main body components

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled>Vue Quiz</b-nav-item>
+            <b-nav-item disabled>Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
+        </b-nav>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: [
+            'numCorrect',
+            'numTotal'
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
